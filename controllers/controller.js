4e36a351c8b14ec05dc9b483f43bbb9e0d66ca45
Joe Thor <joethor1989@gmail.com>
2018-09-22 13:16:47 -0500

    working on the .env

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,18 +1,45 @@
-var db = require("../models");
+const db = require("../models");
+const key = require("../keys");
+import axios from "axios";
+require("dotenv").config();
+const BASEURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
+const APIKEY = key.nyt.id
 
-module.exports = function (app) {
-
-    app.get("/", function (req, res) {
-        db.Article.find({}).sort({date: -1})
-            .then(function (dbArticles) {
-                console.log(dbArticles)
-
-                var hbsObject = {
-                    articles: dbArticles
-                }
-
-                res.render("index", hbsObject);
-
-            })
-    })
+// Defining methods for the Article
+module.exports = {
+  findAll: function(req, res) {
+    db.Article
+      .find(req.query)
+      .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function(req, res) {
+    db.Article
+      .findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  create: function(req, res) {
+    db.Article
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  update: function(req, res) {
+    db.Article
+      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: function(req, res) {
+    db.Article
+      .findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  nytsearch: function(req, res){
+    axios.get(BASEURL + APIKEY)
+  }
 };
---
+++ keys.js
@@ -0,0 +1,8 @@
+//console.log('this is loaded');
+
+console.log('this is loaded');
+
+exports.nyt = {
+  id: process.env.NYT_KEY
+};
+
