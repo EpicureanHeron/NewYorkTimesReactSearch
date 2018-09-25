d8b7aaaf92e473ac89bbb560e7d9e71cfdd30cb3
Joe Thor <joethor1989@gmail.com>
2018-09-24 20:03:16 -0500

    save button posts data to mongodb succesfully

--- client/src/Components/Article/Articleitem.js
+++ client/src/Components/Article/Articleitem.js
@@ -4,7 +4,8 @@ export const Articleitem = props => (
   <li key={props.key} className="list-group-item">
     <a target="_blank" href={props.url}>{props.headline}</a>
     <p>{props.date}</p>
-    <button type="button" onClick={props.handleClick(props.headline, props.url, props.date)} className="btn btn-info">Save</button>
+    <button type="button" onClick={() => props.handleClick(props.headline, props.url, props.date)} className="btn btn-info">Save</button>
   </li>
 );
 export default Articleitem;
+
