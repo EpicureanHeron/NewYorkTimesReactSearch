e40945e259aee4d29f441ef4b85a84163e7b119d
Joe Thor <joethor1989@gmail.com>
2018-09-24 19:26:31 -0500

    able to search and display articles on screen. Working on saving articles to DB via button press. The onclick function being passed to the articleitem is currently auto executing. Same issue as was discovered in class Thursday. Probably needs to be an anonymous function

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,21 +2,21 @@ import axios from "axios";
 const BASEURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
 
 export default {
-  // Gets all books
-  getBooks: function() {
-    return axios.get("/api/books");
+  // Gets all articles
+  getArticles: function() {
+    return axios.get("/api/articles");
   },
-  // Gets the book with the given id
-  getBook: function(id) {
-    return axios.get("/api/books/" + id);
+  // Gets the article with the given id
+  getarticle: function(id) {
+    return axios.get("/api/articles/" + id);
   },
-  // Deletes the book with the given id
-  deleteBook: function(id) {
-    return axios.delete("/api/books/" + id);
+  // Deletes the article with the given id
+  deletearticle: function(id) {
+    return axios.delete("/api/articles/" + id);
   },
-  // Saves a book to the database
-  saveBook: function(bookData) {
-    return axios.post("/api/books", bookData);
+  // Saves a article to the database
+  savearticle: function(articleData) {
+    return axios.post("/api/articles", articleData);
   },
   searchNYT: function(topic){
     let apiURL = `${BASEURL}q=${topic}&api-key=27221a6d54e94979826fb6e63cbd2df8`
